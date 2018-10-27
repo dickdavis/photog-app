8714352c7ad17324997442b3cf9df07ff2068596
Dick Davis <rdavis89@my.apsu.edu>
2018-10-26 20:01:58 -0500

    fixed env variables

--- config/initializers/clearance.rb
+++ config/initializers/clearance.rb
@@ -1,5 +1,5 @@
 Clearance.configure do |config|
-  config.allow_sign_up = <%= ALLOW_SIGNUP %>
-  config.mailer_sender = 'noreply@<%= MAILER_DOMAIN %>'
+  config.allow_sign_up = ENV['ALLOW_SIGNUP']
+  config.mailer_sender = "noreply@#{ENV['MAILER_DOMAIN']}"
   config.rotate_csrf_on_sign_in = true
 end
