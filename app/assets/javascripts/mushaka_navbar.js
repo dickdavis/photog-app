af0cdf2decbff605628d11565d38b99c5fddb0b6
Richard Davis <dick@rvdavis.me>
2019-04-07 02:46:12 +0000

    Implemented JS configuration for linting

--- app/assets/javascripts/mushaka_navbar.js
+++ app/assets/javascripts/mushaka_navbar.js
@@ -1,11 +1,11 @@
-function activateMenu() {
-  let toggle = document.getElementById('MushakaNavbar__navbar-toggle');
-  let menu = document.getElementsByClassName('MushakaNavbar__navbar-menu')[0];
+function activateMenu () {
+  let toggle = document.getElementById('MushakaNavbar__navbar-toggle')
+  let menu = document.getElementsByClassName('MushakaNavbar__navbar-menu')[0]
 
-  toggle.addEventListener('click', toggleMenu);
+  toggle.addEventListener('click', toggleMenu)
 
-  function toggleMenu(e) {
-    e.preventDefault();
-    menu.classList.toggle('MushakaNavbar__navbar-menu_opened');
+  function toggleMenu (e) {
+    e.preventDefault()
+    menu.classList.toggle('MushakaNavbar__navbar-menu_opened')
   }
 }
